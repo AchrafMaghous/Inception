82153379cfd597bbc53b2b261ef63589fe346bb7
AchrafMaghous <muteallfocus7@gmail.com>
2022-11-30 16:06:00 +0100

    changing dependencies

--- srcs/requirements/wordpress/config/setup.sh
+++ srcs/requirements/wordpress/config/setup.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
-wget http://wordpress.org/latest.tar.gz
-tar xfz latest.tar.gz
-mv -f wordpress/* .
-rm -rf latest.tar.gz
-rm -rf wordpress
-
-sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-cp wp-config-sample.php wp-config.php
+apt update -y
+apt install curl php-fpm php-mysql -y
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp 
+sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+wp core download --path="/var/www/html"  --allow-root
+chown -R www-data:www-data /var/www/html
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_host --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
+define('WP_REDIS_HOST', 'redis');
+define('WP_REDIS_PORT', 6379);
+define('WP_REDIS_DISABLED', false);
+PHP
+wp core install --url=$url --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --allow-root --path=/var/www/html
+wp user create acmaghou42 acmaghou42@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
+service php7.3-fpm start
+wp plugin install redis-cache --path=/var/www/html --allow-root
+wp plugin activate redis-cache --path=/var/www/html --allow-root
+wp redis enable --path=/var/www/html --allow-root
+service php7.3-fpm stop
+php-fpm7.3 -F
