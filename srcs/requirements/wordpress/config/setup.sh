bff8b533c5696ee8078df0bcf883e403e479903a
AchrafMaghous <muteallfocus7@gmail.com>
2022-12-02 17:49:22 +0100

    changing dependencies

--- srcs/requirements/wordpress/config/setup.sh
+++ srcs/requirements/wordpress/config/setup.sh
@@ -6,11 +6,7 @@ mv wp-cli.phar /usr/local/bin/wp
 sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 wp core download --path="/var/www/html"  --allow-root
 chown -R www-data:www-data /var/www/html
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_host --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
-define('WP_REDIS_HOST', 'redis');
-define('WP_REDIS_PORT', 6379);
-define('WP_REDIS_DISABLED', false);
-PHP
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_host --path=/var/www/html --allow-root --skip-check
 wp core install --url=$url --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --allow-root --path=/var/www/html
 wp user create acmaghou42 muteallfocus7@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
 service php7.3-fpm start
