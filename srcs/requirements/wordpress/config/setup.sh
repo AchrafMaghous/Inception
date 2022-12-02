adc3801032732cc3f14378f7846a4183af8f440e
AchrafMaghous <muteallfocus7@gmail.com>
2022-12-02 18:04:14 +0100

    changing dependencies

--- srcs/requirements/wordpress/config/setup.sh
+++ srcs/requirements/wordpress/config/setup.sh
@@ -8,7 +8,7 @@ wp core download --path="/var/www/html"  --allow-root
 chown -R www-data:www-data /var/www/html
 wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_host --path=/var/www/html --allow-root --skip-check
 wp core install --url=$url --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --allow-root --path=/var/www/html
-wp user create acmaghou42 muteallfocus7@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
+wp user create achraf muteallfocus7@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
 service php7.3-fpm start
 wp plugin install redis-cache --path=/var/www/html --allow-root
 wp plugin activate redis-cache --path=/var/www/html --allow-root
