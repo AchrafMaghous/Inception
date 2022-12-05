8f75cd0b0bfd36a61908ce45f1c6612cb5606bdc
AchrafMaghous <muteallfocus7@gmail.com>
2022-12-05 15:38:55 +0100

    starting redis wp

--- srcs/requirements/wordpress/config/setup.sh
+++ srcs/requirements/wordpress/config/setup.sh
@@ -9,8 +9,8 @@ chown -R www-data:www-data /var/www/html
 wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_host --path=/var/www/html --allow-root --skip-check
 wp core install --url=$url --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --allow-root --path=/var/www/html
 wp user create achraf muteallfocus7@gmail.com --user_pass=$MYSQL_PASSWORD --role=author --allow-root --path=/var/www/html/
-wp config set WP_REDIS_PATH --raw "__DIR__ . '/../redis.sock'"
-wp config set WP_REDIS_SCHEME "unix"
+wp config set WP_REDIS_PATH --raw "__DIR__ . '/../redis.sock'" --allow-root
+wp config set WP_REDIS_SCHEME "unix" --allow-root
 service php7.3-fpm start
 wp plugin install redis-cache --path=/var/www/html --allow-root
 wp plugin activate redis-cache --path=/var/www/html --allow-root
