f58c19caf5ecdab0027dba1c836753ee68b4f069
AchrafMaghous <muteallfocus7@gmail.com>
2022-12-09 08:56:39 +0100

    changed ftp dockerfile to script

--- srcs/bonus/ftp/config/script.sh
+++ srcs/bonus/ftp/config/script.sh
@@ -2,10 +2,17 @@
 
 apt-get update && apt-get -y install vsftpd
 service vsftpd start
-adduser --disabled-password --gecos "" $FTPUSER\
-mkdir /home/$FTPUSER/ftp\
-chown nobody:nogroup /home/$FTPUSER/ftp && chmod a-w /home/$FTPUSER/ftp \
-mkdir /home/$FTPUSER/ftp/upload \
-chown $FTPUSER:$FTPUSER /home/$FTPUSER/ftp/upload
-echo "$FTPUSER" | tee -a /etc/vsftpd.userlist \
+echo "$FTPUSER" >> /etc/vsftpd.userlist
+echo "local_enable=YES" >> /etc/vsftpd.conf;
+echo "write_enable=YES" >> /etc/vsftpd.conf;
+echo "chroot_local_user=YES" >> /etc/vsftpd.conf;
+echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf;
+echo "pasv_enable=YES" >> /etc/vsftpd.conf;
+echo "pasv_min_port=40000" >> /etc/vsftpd.conf;
+echo "pasv_max_port=40005" >> /etc/vsftpd.conf;
+echo "userlist_enable=YES" >> /etc/vsftpd.conf;
+echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf;
+echo "userlist_deny=NO" >> /etc/vsftpd.conf;
+echo "secure_chroot_dir=/home/$FTPUSER/ftp" >> /etc/vsftpd.conf;
 service vsftpd restart
+/usr/sbin/vsftpd
